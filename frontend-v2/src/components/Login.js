20e4444c2ef1dd8cc4352dc43ae7a6dd2a17c657
Ethan Peterson <ethan@petetech.net>
2019-06-23 07:38:56 -0400

    add material ui pkg

---
+++ frontend-v2/src/components/Login.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: null,
+      password: null,
+    }
+  }
+
+  emailChanged = (event) => {
+    console.log(event.target.value)
+    this.setState({ email: event.target.value })
+  }
+
+  passwordChanged = (event) => {
+    console.log(event.target.value)
+    this.setState({ password: event.target.value })
+  }
+
+  render() {
+    return (
+      <form noValidate autoComplete="off">
+        <div>
+          <img src="logo.png"></img>
+        </div>  
+        <div>
+          <TextField
+            label="Email"
+            type="email"
+            autoComplete="email"
+            margin="normal"
+            variant="filled"
+            onChange={this.emailChanged}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+            variant="filled"
+            onChange={this.passwordChanged}
+          />
+        </div>
+        <div>
+          <Button variant="contained" color="primary" onClick={this.props.handler}>
+            Login
+          </Button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default LoginPage;
