91fd8ff3512f4df33c917501ba78ba51c08c3daa
Ethan Peterson <ethan@petetech.net>
2019-06-23 02:51:50 -0400

    add new sql lib dependency

--- backend/AuthController.js
+++ backend/AuthController.js
@@ -1,18 +1,34 @@
-const jwt = require('jsonwebtoken');
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
-function verifyToken(req, res, next) {
-    var token = req.headers['x-access-token'];
-    if (!token)
-        return res.status(403).send({ auth: false, message: 'No token provided.' });
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
-    jwt.verify(token, process.env.SECRET, function (err, decoded) {
-        if (err)
-            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+var VerifyToken = require('./VerifyToken');
 
-        // if everything good, save to request for use in other routes
-        req.userId = decoded.id;
-        next();
-    });
-}
+router.post('/register', function (req, res) {
 
-module.exports = verifyToken;
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+    User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        type: req.body.type,
+        payments_assigned: [],
+        password: hashedPassword
+    },
+        function (err, user) {
+            if (err) return res.status(500).send("There was a problem registering the user.")
+
+            // create a token
+            var token = jwt.sign({ id: user._id }, process.env.SECRET, {
+                expiresIn: 86400 // expires in 24 hours
+            });
+
+            res.status(200).send({ auth: true, token: token });
+        });
+});
