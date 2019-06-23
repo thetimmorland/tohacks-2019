9eb23e2fc9bc27e608d4d3f47c873c28bcb24be2
Ethan Peterson <ethan@petetech.net>
2019-06-23 00:21:53 -0400

    add static fileserver for frontend dashboard build

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// API ENDPOINTS HERE (always lead route with API)
+app.get('/api', function (req, res) {
+    res.send('Hello World!')
+})
+
+
+// Serve static Frontend dashboard
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function(req, res) { // * char allows for internal routing in our frontend
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
