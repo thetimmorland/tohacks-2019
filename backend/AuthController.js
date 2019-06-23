1b25d805cdd204f9c04fb287986c70610443933f
Ethan Peterson <ethan@petetech.net>
2019-06-23 02:27:14 -0400

    grab encryption secret from env vars

--- backend/AuthController.js
+++ backend/AuthController.js
@@ -1,12 +1,11 @@
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
     var token = req.headers['x-access-token'];
     if (!token)
         return res.status(403).send({ auth: false, message: 'No token provided.' });
 
-    jwt.verify(token, config.secret, function (err, decoded) {
+    jwt.verify(token, process.env.SECRET, function (err, decoded) {
         if (err)
             return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
