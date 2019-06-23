6ec26288c9870ac86482819e3e24ceaf9a22b36f
Ethan Peterson <ethan@petetech.net>
2019-06-23 07:54:45 -0400

    registration page routed

--- frontend-v2/src/containers/RegisterPage.js
+++ frontend-v2/src/containers/RegisterPage.js
@@ -1,65 +1,14 @@
-import React, { Component } from "react";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import React from 'react'
+import Register from '../components/Register'
 
-class RegisterPage extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      email: null,
-      password: null,
+class RegisterPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <Register />
+            </div>
+        )
     }
-  }
-
-  emailChanged = (event) => {
-    console.log(event.target.value)
-    this.setState({ email: event.target.value })
-  }
-
-  passwordChanged = (event) => {
-    console.log(event.target.value)
-    this.setState({ password: event.target.value })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <TextField
-            label="Email"
-            type="email"
-            autoComplete="email"
-            margin="normal"
-            variant="filled"
-            onChange={this.emailChanged}
-          />
-        </div>
-        <div>
-          <TextField
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            margin="normal"
-            variant="filled"
-            onChange={this.passwordChanged}
-          />
-        </div>
-        <div>
-          <TextField
-            label="Confirm Password"
-            type="password"
-            autoComplete="current-password"
-            margin="normal"
-            variant="filled"
-          />
-        </div>
-        <div>
-          <Button variant="contained" color="primary">Register</Button>
-        </div>
-      </div>
-    );
-  }
 }
 
-export default RegisterPage;
+export default RegisterPage
