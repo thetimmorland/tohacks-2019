34ff1ec4170ddd5f708e998db609a0095e380747
Timothy Morland <thetimmorland@gmail.com>
2019-06-23 03:36:04 -0400

    removed testing code

--- backend/index.js
+++ backend/index.js
@@ -19,34 +19,6 @@ app.use('/api/jobs', JobRoutes)
 // import user only after env variables have been enabled
 const User = require('./models/user').User
 
-// API ENDPOINTS HERE (always lead route with API)
-app.get('/api/register', function (req, res) { // create a fake user example
-    User.create({
-        firstName: 'Timbo4',
-        lastName: 'More Land',
-        email: 'test@test.com',
-        password: 'hunter2',
-        userType: 'student',
-        interests: 'yeet',
-        skills: 'N/A'
-    }).then(user => {
-        console.log(user)
-    })
-    res.send('test registration')
-})
-
-const Posting = require('./models/posting').Posting
-//Posting.sync()
-app.get('/api/posting', function (req, res) {
-    res.send('test posting creation')
-})
-
-const Job = require('./models/job').Job
-//Job.sync()
-app.get('/api/jobs', function (req, res) {
-    res.send('test job creation')
-})
-
 // Serve static Frontend dashboard
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) { // * character allows for internal routing in our frontend
