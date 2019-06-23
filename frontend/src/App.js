73da32c3d0319a95a580d5e1fc080fded5630723
Nathan Lai <17nkhl1@queensu.ca>
2019-06-23 00:44:10 -0400

    added loginpage, profilepage, registrationPage and a topbar for home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from "./LoginPage.js";
+import RegisterPage from "./RegisterPage.js";
+import Home from "./Home.js";
+import Profile from "./Profile.js";
+import TopBar from "./TopBar.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Home/>
+      </div> 
     );
   }
 }
---
+++ frontend/src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import TopBar from "./TopBar.js";
+
+
+class Home extends Component {
+
+  render() {
+    return (
+        <div>
+            <TopBar/>
+        </div>
+    );
+  }
+}
+
+export default Home;
