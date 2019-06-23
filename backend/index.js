da0248e197628257eaddaa82ef9e04c7a0cf804b
Timothy Morland <thetimmorland@gmail.com>
2019-06-23 00:02:36 -0400

    got it working

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,17 @@ if (!process.env.DB_USER || !process.env.DB_PASS) {
 
 // SEQUELIZE TEST
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    })
+
+sequelize.authenticate().then(() => {
+    console.log('success')
 })
 
 class User extends Sequelize.Model {}
