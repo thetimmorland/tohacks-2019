9564bf022bbdf32a2f2b39628b181908e8efe529
Timothy Morland <thetimmorland@gmail.com>
2019-07-28 13:50:00 -0400

    sync db before doing anything else

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
+const sequelize = require('./models').sequelize;
+sequelize.sync();
+
 const express = require('express');
 const app = express();
 
 const users = require('./routes/users');
 const jobs = require('./routes/jobs');
 const postings = require('./routes/postings');
-
-const sequelize = require('./models').sequelize;
-sequelize.sync();
 
 app.use('/api/jobs', jobs);
 app.use('/api/users', users);
