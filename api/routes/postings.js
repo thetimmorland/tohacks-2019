5e0fc6eacb55979e34d760100e921de369fc7df3
Timothy Morland <thetimmorland@gmail.com>
2019-07-31 06:47:28 -0400

    add error handling for postings route

--- api/routes/postings.js
+++ api/routes/postings.js
@@ -10,25 +10,30 @@ const Posting = require('../models').Posting;
 router.route('/')
   .post((req, res) => {
     Posting.create(req.body)
-      .then(posting => res.status(200).send({ id: posting.id }));
+      .then(posting => res.status(200).send({ id: posting.id }))
+      .catch(error => res.status(400).send(error));
   })
   .get((req, res) => {
     Posting.findAll()
-      .then(postings => res.status(200).send(postings));
+      .then(postings => res.status(200).send(postings))
+      .catch(error => res.status(400).send(error));
   });
 
 router.route('/:id')
   .get((req, res) => {
     Posting.findOne({ where: { id: req.params.id }})
-      .then(posting => res.status(200).send(posting));
+      .then(posting => res.status(200).send(posting))
+      .catch(error => res.status(400).send(error));
   })
   .put((req, res) => {
     Posting.update(req.body, { where: { id: req.params.id } })
-      .then(() => res.sendStatus(200));
+      .then(res.sendStatus(200))
+      .catch(error => res.status(400).send(error));
   })
   .delete((req, res) => {
     Posting.destroy({ where: { id: req.params.id } })
-      .then(() => res.sendStatus(200));
+      .then(res.sendStatus(200))
+      .catch(error => res.status(400).send(error));
   });
 
 module.exports = router;
