6df0e65141844cb792a6ffd367d2bff5b4ba5c7b
Timothy Morland <thetimmorland@gmail.com>
2019-07-31 06:55:27 -0400

    add error handling to volunteers route

--- api/routes/volunteers.js
+++ api/routes/volunteers.js
@@ -10,27 +10,30 @@ const Volunteer = require('../models').Volunteer;
 router.route('/')
   .post((req, res) => {
     Volunteer.create(req.body)
-      .then(volunteer => res.status(200).send({ id: volunteer.id }));
+      .then(volunteer => res.status(200).send({ id: volunteer.id }))
+      .catch(error => res.status(400).send(error));
   })
   .get((req, res) => {
     Volunteer.findAll()
-      .then(volunteers => res.status(200).send(volunteers));
+      .then(volunteers => res.status(200).send(volunteers))
+      .catch(error => res.status(400).send(error));
   });
 
 router.route('/:id')
   .get((req, res) => {
     Volunteer.findOne({ where: { id: req.params.id } })
-      .then(user => res.status(200).send(users));
+      .then(user => res.status(200).send(users))
+      .catch(error => res.status(400).send(error));
   })
   .put((req, res) => {
-    Volunteer.update(
-      req.body,
-      { where: { id: req.params.id } }
-    ).then(() => res.sendStatus(200));
+    Volunteer.update(req.body, { where: { id: req.params.id } })
+      .then(res.sendStatus(200))
+      .catch(error => res.status(400).send(error));
   })
   .delete((req, res) => {
     Volunteer.destroy({ where: { id: req.params.id }})
-      .then(() => res.sendStatus(200));
+      .then(() => res.sendStatus(200))
+      .catch(error => res.status(400).send(error));
   });
 
 module.exports = router;
